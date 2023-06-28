6d7b44ce28ad464961c64285e7b571f4171f3a87
TestName <48367659+SufferedComet@users.noreply.github.com>
2023-06-27 18:00:55 -0700

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Angelo Delgado on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
